c6235d3c53197f5ee827660b0ded98b396c3bfc6
uditya kumar <rishi95122@gmail.com>
2024-10-10 17:01:53 +0530

    DAS

--- index.js
+++ index.js
@@ -2,10 +2,11 @@ import express from "express";
 import { PrismaClient } from "@prisma/client";
 import userRoutes from "./routes/userRoutes.js";
 import videoRoutes from "./routes/videoRoutes.js";
-
+import cors from "cors";
 const prisma = new PrismaClient();
 const app = express();
 app.use(express.json());
+app.use(cors());
 const PORT = process.env.PORT || 3000;
 
 app.use("/api/users", userRoutes);
