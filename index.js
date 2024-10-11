98e6ee931e07a5018b29087df09bb1222823d16d
uditya kumar <rishi95122@gmail.com>
2024-10-11 09:31:59 +0530

    S

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 import express from "express";
 import { PrismaClient } from "@prisma/client";
-import cors from "cors";  // Import CORS
+import cors from "cors"; // Import CORS
 import userRoutes from "./routes/userRoutes.js";
 import videoRoutes from "./routes/videoRoutes.js";
-import cors from "cors";
+
 const prisma = new PrismaClient();
 const app = express();
 
@@ -12,19 +12,13 @@ app.use(cors());
 
 const PORT = process.env.PORT || 3000;
 
-// Enable CORS
 app.use(cors());
 
-// Middleware to parse JSON
 app.use(express.json());
 
-
-
-// Routes
 app.use("/api/users", userRoutes);
 app.use("/api/videos", videoRoutes);
 
-// Start the server
 app.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
 });
